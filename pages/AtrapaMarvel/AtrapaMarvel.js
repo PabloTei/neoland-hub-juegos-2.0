af0c3520141efbe93ffa33e151ada2b7855187b3
PabloTei <pteijeirolopez@gmail.com>
2023-02-06 17:01:44 +0100

    atrapaAMarvel

--- pages/AtrapaMarvel/AtrapaMarvel.js
+++ pages/AtrapaMarvel/AtrapaMarvel.js
@@ -4,14 +4,15 @@ export const AtrapaMarvel = () => {
     const container = document.querySelector("main");
     container.innerHTML = `
     <h1 class"titulo-marvel">¡ATRAPA A MARVEL!</h1>
-    <h2>Marvel atrapado:</h2>
-    <h2 id="puntaje">0</h2>
+    <h2>PUNTOS:</h2>
+    <h2 id="puntuacion">0</h2>
     <h2>Tiempo:</h2>
-    <h2 id="tiempo">50</h2>
+    <h2 id="TIEMPO">30</h2>
     <div class="cuadricula"></div>
     `
     meterDivs();
     movMarvel();
+    //touchMarvel();
     
 }
 
@@ -36,21 +37,42 @@ const posicionRandom = () => {
     return cuadroConcreto;
 }
 
+
+let resultado = 0;
+
 const printMarvel = () => {
+    const puntuacion = document.querySelector("#puntuacion");
     const remove = document.querySelectorAll(".cuadro");
     for (const item of remove) {
         item.innerHTML = "";
     }
-    posicionRandom().innerHTML = `
-    <img class="img-marvel" src="https://res.cloudinary.com/depifliz3/image/upload/v1675642653/samples/animals/IMG_1307_jedwjt.png">
-    `
+    const imagen = document.createElement("img");
+    imagen.setAttribute("src", "https://res.cloudinary.com/depifliz3/image/upload/v1675642653/samples/animals/IMG_1307_jedwjt.png");
+    imagen.setAttribute("class", "img-marvel")
+    posicionRandom().appendChild(imagen);
+    imagen.addEventListener("click", () => {
+        resultado ++;
+        puntuacion.innerHTML = `
+        ${resultado}
+        `
+    })
 }
-
 
 const movMarvel = () => {
     const tiempoMarvel = setInterval(printMarvel, 1000);
     return tiempoMarvel;
 }
+
+
+
+
+
+
+
+    
+
+
+
 
 
 
